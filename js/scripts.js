8db7695d56a3306e4df1ebba83f185b044c05564
ronald <r.gaskin@live.ca>
2019-05-29 09:50:24 -0700

    meta-work

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+(function($) {
+  $('#toggle-status').on('click', function() {
+    event.preventDefault();
+
+    $.ajax({
+      method: 'post',
+      url: api_vars.rest_url + 'wp/v2/posts/' + api_vars.post_id,
+      data: { post_status: 'draft' },
+      beforeSend: function(xhr) {
+        xhr.setRequestHeader('x-wp-nonce', api_vars.wpapi_nonce);
+      }
+    }).done(function() {
+      alert('Success! The status has been changed.');
+    });
+  });
+})(jQuery);
+
+// beginning of example
+
+// ajax old school version
+
+// (function($) {
+//     $('#toggle-status').on('click', function() {
+//       event.preventDefault();
+
+//       $.ajax({
+//         method: 'post',
+//         url: api_vars.ajax_url,
+//         data: {
+//           action: 'qod-status-ajax',
+//           secrity: api_vars.status_nonce,
+//           the_post_id: api_vars.post_id
+//         }
+//       }).done(function(response) {
+//         alert('Success! The status has been changed.');
+//       });
+//     });
+//   })(jQuery);
+
+// end of example
--- template-parts/content.php
+++ template-parts/content.php
@@ -4,12 +4,17 @@
  *
  * @package QOD_Starter_Theme
  */
+$source = get_post_meta(get_the_ID(), '_qod_quote_source', true);
+$source_url = get_post_meta(get_the_ID(), '_qod_quote_source_url', true);
 
+var_dump($source);
 ?>
+
+
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+		<?php the_title(sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>'); ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
