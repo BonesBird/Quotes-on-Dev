5a636382984dcff9bcc857b2ff9ac244f4f0585c
ronald <r.gaskin@live.ca>
2019-05-30 09:46:38 -0700

    building-pages

--- footer.php
+++ footer.php
@@ -7,16 +7,33 @@
 
 ?>
 
-			</div><!-- #content -->
+</div><!-- #content -->
 
-			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
-				</div><!-- .site-info -->
-			</footer><!-- #colophon -->
-		</div><!-- #page -->
+<footer id="colophon" class="site-footer" role="contentinfo">
+	<div class="site-info">
+		<a href="<?php echo esc_url('https://wordpress.org/'); ?>"><?php printf(esc_html('Proudly powered by %s'), 'WordPress'); ?></a>
+		<section class="footer-qod">
+			<ul>
+				<Nav class="footer-nav">
+					<li>
+						<div class="about">About</div>
+					</li>
+					<li>
+						<div class="archive">Archive</div>
+					</li>
+					<li>
+						<div class="submit-a-quote">Submit a quote</div>
+					</li>
+				</Nav>
+			</ul>
+		</section>
+	</div><!-- .site-info -->
 
-		<?php wp_footer(); ?>
+</footer><!-- #colophon -->
+</div><!-- #page -->
 
-	</body>
+<?php wp_footer(); ?>
+
+</body>
+
 </html>
