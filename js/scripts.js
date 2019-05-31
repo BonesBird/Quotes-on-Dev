c510bd4b7b57d07a37bdbc4fd92fff4081dc9c9e
ronald <r.gaskin@live.ca>
2019-05-31 15:14:19 -0700

    button-trouble-shoot

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
-(function($) {
-  $('#toggle-status').on('click', function() {
-    event.preventDefault();
+// (function($) {
+//   $('#toggle-status').on('click', function() {
+//     event.preventDefault();
 
-    $.ajax({
-      method: 'post',
-      url: api_vars.rest_url + 'wp/v2/posts/' + api_vars.post_id,
-      data: { post_status: 'draft' },
-      beforeSend: function(xhr) {
-        xhr.setRequestHeader('x-wp-nonce', api_vars.wpapi_nonce);
-      }
-    }).done(function() {
-      alert('Success! The status has been changed.');
-    });
-  });
-})(jQuery);
+//     $.ajax({
+//       method: 'post',
+//       url: api_vars.rest_url + 'wp/v2/posts/' + api_vars.post_id,
+//       data: { post_status: 'draft' },
+//       beforeSend: function(xhr) {
+//         xhr.setRequestHeader('x-wp-nonce', api_vars.wpapi_nonce);
+//       }
+//     }).done(function() {
+//       alert('Success! The status has been changed.');
+//     });
+//   });
+// })(jQuery);
 
 // beginning of example
 
@@ -39,27 +39,66 @@
 
 // end of example
 
-(function() {
+(function($) {
   //doc ready short hand...
   $(function() {
+    let lastPage = '';
+    //events
+    $('#quote-submission-form').on('submit', postQuote);
+
     $('#new-quote-button').on('click', getRandomQuote);
+
     function getRandomQuote(event) {
+      event.preventDeafult();
+
+      lastPage = document.URL;
+
       $.ajax({
         method: 'get',
         url:
           api_vars.rest_url +
           'wp/v2/posts?filter[orderby]=rand&filter[posts_per_page]=1'
       })
+
         .done(function(data) {
           const randomQuote = data[0];
           history.pushState(null, null, randomQuote.slug);
+          $('#quote-submission-form').slideUp(500);
 
           //return the dom array
         })
+
         .fail(function(error) {
-          console.log(lastPage);
+          console.log(error);
         });
+
+      $(window).on('popstate', function() {
+        window.location.replace(lastPage);
+      });
     }
-  });
-  //end of doc ready
-})(jQuery);
+    function postQuote(event) {
+      event.preventDeafult();
+      console.log('form submitted');
+      const quoteAuthor = $('#quote-author').val();
+      if(quoteAuthor !== '')
+      postAjax();
+    }
+    function postAjax(){
+    $.ajax({
+              method: 'post',
+              url: api_vars.ajax_url + 'wp/v2posts',
+              data: {
+                title:'A new post',
+                content: 'the most amazing quote by gordan ramsey',
+                status: 'pending'
+                // _qod_quote_source:
+                // qod_quote_url:
+              },beforeSend: function(xhr){
+                xhr.setRequestHeader('X-WP-wpapi_nonce');
+              }
+
+  )}).done(function){
+
+  }).fail(function)(){
+
+  });})}(jQuery);
